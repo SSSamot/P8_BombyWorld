6acd3c529282ab97d750c08adf77a62f2f93fc1d
Azrod1 <91877581+Azrod1@users.noreply.github.com>
2022-04-19 08:55:43 +0200

    Bomb fonctionnel

--- Assets/Scripts/Bomb/BehaviorBomb.cs
+++ Assets/Scripts/Bomb/BehaviorBomb.cs
@@ -3,17 +3,22 @@ using UnityEngine;
 
 public class BehaviorBomb : MonoBehaviour
 {
-    public int timer; 
+    public int timer;
+    public bool explosion = false;
 
     private AnimBomb anim;
+
+    public GameObject player;
 
     void Start()
     {
         anim = GetComponent<AnimBomb>();
 
-        timer = 10;
+        timer = 5;
         StartCoroutine(Timer());
     }
+
+    // timer after explosed bomb
     IEnumerator Timer()
     {
         int timerbis = timer;
@@ -22,6 +27,49 @@ public class BehaviorBomb : MonoBehaviour
             yield return new WaitForSeconds(1.0f);
             timer -= 1;
         }
+        Explosion();
+    }
+
+    // anim explosion + block movement + call DestroyBomb()
+    void Explosion()
+    {
         anim.ExplosionBomb();
+        explosion = true;
+        StartCoroutine(DestroyBomb());
+        //KillPlayer();
+    }
+
+    // destroy bomb after 2sec
+    IEnumerator DestroyBomb()
+    {
+        yield return new WaitForSeconds(1.0f);
+        KillPlayer();
+        yield return new WaitForSeconds(1.0f);
+        Destroy(this.gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player Detected");
+            player = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            player = null;
+        }
+    }
+
+    void KillPlayer()
+    {
+        if(player != null)
+        {
+            Destroy(player);
+        }
     }
 }
--- Assets/Scripts/Bomb/MoveBomb.cs
+++ Assets/Scripts/Bomb/MoveBomb.cs
@@ -21,20 +21,23 @@ public class MoveBomb : MonoBehaviour
     }
     void Update()
     {
-        agent.SetDestination(target.position);
-
-        player = target.position;
-        bomb = transform.position;
-
-        if (Vector3.Distance(player, bomb) < 3f)
+        if (!behavior.explosion)
         {
-            agent.speed = 0f;
-            anim.IdleBomb();
-        }
-        else
-        {
-            agent.speed = 3.5f;
-            anim.WalkBomb();
+            agent.SetDestination(target.position);
+
+            player = target.position;
+            bomb = transform.position;
+
+            if (Vector3.Distance(player, bomb) < 3f)
+            {
+                agent.speed = 0f;
+                anim.IdleBomb();
+            }
+            else
+            {
+                agent.speed = 3.5f;
+                anim.WalkBomb();
+            }
         }
     }
 }
